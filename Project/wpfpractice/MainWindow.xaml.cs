ef20712134c87eb29e94821f98da28baab9483fc
Ibtehaj778 <ibtehaja77@gmail.com>
2024-03-08 22:20:21 +0500

    adding buttons

---
+++ Project/wpfpractice/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfpractice
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool Mouse_Enter = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void HOME_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if(Mouse_Enter)
+            {
+
+            }
+        }
+    }
+}
